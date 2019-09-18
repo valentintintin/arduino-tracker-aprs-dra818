5ca55d955b8dc2afe2f1bd79c43df544d4b20faf
valentintintin <valentin.s.10@gmail.com>
2019-09-18 12:19:22 +0200

    :ok_hand: Updating pin TX/RX DRA name #8

--- src/main.cpp
+++ src/main.cpp
@@ -20,15 +20,15 @@
 #endif
 
 #define DRA_PTT 2
-#define TX_GPS 6
-#define RX_DRA 3
-#define TX_DRA 4
+#define TX_GPS 6 // from GPS to Arduino
+#define TX_DRA 3 // from DRA to Arduino
+#define RX_DRA 4 // from Arduino to DRA
 #define DRA_ACTIVE 5
 //#define BUTTON 12
 #define PTT_OUT 12
 
 char CALL[] = "F4HVV";
-uint8_t CALL_ID = '9';
+uint8_t callSsid = '9'; // car
 char TO_CALL[] = "APRS";
 uint8_t TO_CALL_ID = '0';
 char RELAYS[] = "WIDE1-1,WIDE2-2";
@@ -36,7 +36,7 @@ char RELAYS[] = "WIDE1-1,WIDE2-2";
 bool isTestMode = IS_TEST_MODE;
 
 GPS gps(TX_GPS);
-DRA dra(RX_DRA, TX_DRA, DRA_PTT, DRA_ACTIVE);
+DRA dra(TX_DRA, RX_DRA, DRA_PTT, DRA_ACTIVE);
 APRS aprs(&dra, &gps, TX_TIME_BETWEEN, TX_SPEED_DIFFERENCE, TX_LOCATION_DIFFERENCE, PTT_OUT);
 
 void setup() {
@@ -49,10 +49,10 @@ void setup() {
     DPRINTLN(F("Starting ..."));
 
     if (!dra.init(TX_FREQ)) {
-        CALL_ID = '7';
+        callSsid = '7'; // walk
     }
 
-    aprs.init(CALL, CALL_ID, TO_CALL, TO_CALL_ID, RELAYS);
+    aprs.init(CALL, callSsid, TO_CALL, TO_CALL_ID, RELAYS);
 
     if (!dra.isDraDetected()) {
         aprs.setTimeBetweenTx(TX_TIME_BETWEEN_NO_DRA);
